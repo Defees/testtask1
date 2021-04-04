a36b6e0d0fe1c4019c536e5ee9559196f9ed57e4
Ivan Bartov <vanya.bartov@gmail.com>
2021-04-04 19:32:00 +0300

    commit

--- src/even-numbers.js
+++ src/even-numbers.js
@@ -1,6 +1,21 @@
 const evenNumbersInArray = (array) => {
-    // Your implementation
-    // Read README.md file, if you not understand what to do
+    if(Array.isArray(array) && array.length > 0){
+        var tempArr = [];
+        for (let i = 0; i < array.length; i++) {
+            if(array[i]%2 == 0){
+                tempArr.push(array[i]);
+            }
+        }
+        if(tempArr.length == 0){
+            return "Passed array does not contain even numbers";
+        }
+        else{
+            return tempArr;
+        }
+    }
+    else{
+        return "Passed argument is not an array or empty";
+    }
 };
 
 module.exports = evenNumbersInArray;
